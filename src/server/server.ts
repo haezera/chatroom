3e9addb00d6fe67815f7bc5d6fbdc60b8dea10ce
haezera <z5480978@ad.unsw.edu.au>
2023-12-22 20:38:40 +0900

    initial commit

---
+++ src/server/server.ts
@@ -0,0 +1,13 @@
+import express, { Request, Response } from 'express';
+import { createServer } from 'http';
+
+const app = express();
+const server = createServer(app);
+
+app.get('/', (req: Request, res: Response) => {
+  res.send("<h1>You have accessed the root!</h1>");
+});
+
+server.listen(3001, () => {
+  console.log("server running at http://localhost:3001");
+})
