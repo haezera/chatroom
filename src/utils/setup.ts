f454711d06745057d63ea48e7bdc99e37adad6f0
haezera <z5480978@ad.unsw.edu.au>
2023-12-26 23:23:08 +0900

    sql: mysql setup function completed - tables and databases sufficiently being created. currently, the database reloads every single time the server starts for testing simplicity

---
+++ src/utils/setup.ts
@@ -0,0 +1,64 @@
+export const setupSQL = (connection: any) => {
+  connection.query(
+    'DROP DATABASE IF EXISTS chatroom',
+    (err: any, results: any) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log('Database dropped');
+      }
+    }
+  );
+
+  connection.query(
+    'CREATE DATABASE IF NOT EXISTS chatroom',
+    (err: any, results: any) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log('Database created');
+      }
+    }
+  );
+
+  connection.query(
+    'USE chatroom',
+    (err: any, results: any) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log('Database selected');
+      }
+    }
+  );
+
+  connection.query(
+    `CREATE TABLE IF NOT EXISTS users(
+      id INT AUTO_INCREMENT PRIMARY KEY,
+      username VARCHAR(255),
+      password VARCHAR(255),
+      email VARCHAR(255)
+    )`,
+    (err: any, results: any) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log('Table created');
+      }
+    }
+  );
+
+  connection.query(
+    `CREATE TABLE IF NOT EXISTS rooms(
+      room_id VARCHAR(255),
+      username VARCHAR(255)
+    )`,
+    (err: any, results: any) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log('Table created');
+      }
+    }
+  );
+};
