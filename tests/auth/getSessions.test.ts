5b2c1b6ac3423400f621a3a9a744898857de1639
haezera <z5480978@ad.unsw.edu.au>
2024-01-08 21:52:04 +0900

    new: get session stubs for tests

---
+++ tests/auth/getSessions.test.ts
@@ -0,0 +1,13 @@
+import { requestGetSessions } from "../utils/requestHelper";
+import { requestUserCreate } from "../utils/requestHelper";
+import { requestClear } from "../utils/requestHelper";
+
+describe('Get sessions test', () => {
+  test('Success', () => {
+
+  });
+
+  test('Admin password is not correct', () => {
+
+  });
+});
