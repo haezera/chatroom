36a1cb98b01efab25b4338eaf7f559260b54cfb2
metara <jeff.j.lu2@gmail.com>
2024-01-14 03:16:46 +1100

    made logout button and register form

--- src/client/components/Dashboard.tsx
+++ src/client/components/Dashboard.tsx
@@ -1,13 +1,18 @@
 'use strict';
 
 import React from 'react';
+import { AppState } from '../AppState';
+import Logout from './Logout';
 
 const Dashboard = () => {
   console.log("rendering Dashboard");
+  
+  const {view, setView} = React.useContext(AppState);
 
   return (
     <div>
       <h1>Dashboard</h1>
+      <Logout />
     </div>
   );
 };
--- src/client/components/Register.tsx
+++ src/client/components/Register.tsx
@@ -6,11 +6,91 @@ const Register = () => {
 
   const {view, setView} = React.useContext(AppState);
 
+  const [emailInput, setEmailInput] = React.useState<string>("");
+  const [usernameInput, setUsernameInput] = React.useState<string>("");
+  const [passwordInput, setPasswordInput] = React.useState<string>("");
+
+  /// Functions
+
+  const handleSubmit = () => {
+    const responsePromise = fetch(
+      `http://${window.location.hostname}:${window.location.port}/v1/auth/user/create`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json',
+        },
+        body: JSON.stringify({
+          email: emailInput,
+          password: passwordInput,
+          username: usernameInput,
+        }),
+      }
+    );
+    responsePromise.then((rawResponse : Response) => {
+      const jsonResponsePromise: Promise<{session: string}> = rawResponse.json();
+
+      jsonResponsePromise.then((jsonResponse : {session: string}) => {
+        if (rawResponse.status == 200) {
+          // upon successful login
+          const session = jsonResponse.session;
+          window.localStorage.setItem("session", session);
+          console.log("user registered");
+          console.log("session: ", session);
+          setView("dashboard");
+        } else {
+          // upon failed login
+          console.log("registration failed");
+          window.alert("registration failed");
+        }
+      }).catch((err) => {
+        console.error(err);
+        window.alert("registration failed due to error");
+      });
+    }).catch((err) => {
+      console.error(err);
+      window.alert("registration failed due to error");
+    });
+  }
+
+  /// Return statement
+
   return (
     <div>
       <div>
         <h1>Register</h1>
-        <p>insert form here</p>
+        <form name="register-form">
+          <input
+            placeholder = "email"
+            className = 'email-register-input-box'
+            onChange = {(event : React.ChangeEvent) => {
+              setEmailInput(event.target.value);
+            }}
+          />
+          <input
+            placeholder = "username"
+            className = 'username-register-input-box'
+            onChange = {(event : React.ChangeEvent) => {
+              setUsernameInput(event.target.value);
+            }}
+          />
+          <input
+            placeholder = "password"
+            className = 'password-register-input-box'
+            onChange = {(event : React.ChangeEvent) => {
+              setPasswordInput(event.target.value);
+            }}
+          />
+          {React.createElement('input', {
+            type: 'submit',
+            value: 'Submit',
+            className: 'register-submit-button',
+            onClick: (event : React.MouseEvent<HTMLButtonElement>) => {
+              event.preventDefault();
+              handleSubmit();
+            }
+          })}
+        </form>
       </div>
       <div>
         <p>Already have an account?</p>
